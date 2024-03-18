a50452112961541c9755f8d8aec512c7fdad5aa4
Lucas Saintarbor <lucas.saintarbor@suse.com>
2024-03-18 14:46:23 -0700

    Update base url via index.js (#532)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home(context) {
-  return <Redirect to={`${context.config.baseUrl}v1.2`} />;
+  return <Redirect to={`${context.config.baseUrl}v1.3`} />;
 }
